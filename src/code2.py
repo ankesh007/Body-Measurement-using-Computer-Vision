81ee0fafa211fa3b7e27e9ed7c90434364747f57
ankesh007 <ankeshgupta007@gmail.com>
2017-11-26 22:20:49 +0530

    cODE

--- src/code2.py
+++ src/code2.py
@@ -44,6 +44,7 @@ def get_distance(image):
 			break
 
 		key = cv2.waitKey(1) & 0xFF
+	cv2.destroyAllWindows()
 
 	if(len(refPt)==2):
 		print refPt
@@ -68,7 +69,7 @@ def chess_board_corners(image,gray,r):
 	coordinates.append((corners2[square_size-1,0,0],corners2[square_size-1,0,1]))
 	coordinates.append((corners2[rectangle_row*(square_size-1),0,0],corners2[rectangle_row*(square_size-1),0,1]))
 	coordinates.append((corners2[rectangle_row*(square_size-1)+square_size-1,0,0],corners2[rectangle_row*(square_size-1)+square_size-1,0,1]))
-	print coordinates
+	# print coordinates
 	return coordinates
 
 # receives an image and performs affine transform using chess_board_corners
@@ -85,6 +86,24 @@ def affine_correct(image):
 	M=cv2.getPerspectiveTransform(pt1,pt2)
 	dst=cv2.warpPerspective(image,M,(image.shape[1],image.shape[0]))
 	return dst
+
+# returns segmented image around refPt
+def grub_cut(img,refPt):
+	mask = np.zeros(img.shape[:2],np.uint8)
+	bgdModel = np.zeros((1,65),np.float64)
+	fgdModel = np.zeros((1,65),np.float64)
+	# print refPt
+	# refPt[1]=(1000,1500)
+	rect = (refPt[0][0],refPt[0][1],refPt[1][0],refPt[1][1])
+	# print rect
+	# print "Image shape-",img.shape
+	cv2.imwrite("hey.jpg",img)
+	cv2.grabCut(img,mask,rect,bgdModel,fgdModel,10,cv2.GC_INIT_WITH_RECT)
+	mask2 = np.where((mask==2)|(mask==0),0,1).astype('uint8')
+	img = img*mask2[:,:,np.newaxis]
+	return img
+
+
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True, help="Path to the image")
@@ -109,20 +128,48 @@ if(affine_correct_flag=='True'):
 gray2 = cv2.cvtColor(dst,cv2.COLOR_BGR2GRAY) 
 temp=chess_board_corners(dst,gray2,r2)
 
+
 ret, corners = cv2.findChessboardCorners(dst, (rectangle_row,rectangle_col),None)
 corners2 = cv2.cornerSubPix(gray2,corners,(11,11),(-1,-1),criteria)
-print corners2
+# print corners2
 dst = cv2.drawChessboardCorners(dst, (9,6), corners2, ret)
-
 
 cv2.imshow(window_name1,dst)
 metre_pixel_x=(r2*ref_ht)/(abs(temp[0][0]-temp[1][0]))
 metre_pixel_y=(r2*ref_ht)/(abs(temp[0][1]-temp[2][1]))
-print get_distance(dst)
+#print get_distance(dst)
+print "Hi"
+# sep=(coordinate[1][0]-coordinate[0][0])
 
+coordinate=[temp[0],temp[1]]
+# 6X6 square co-ord
+
+sep=((coordinate[1][0]-coordinate[0][0])/6.0)*9.0
+print dst.shape[1]
+print sep
+# exit(1)
+coordinate[0]=(max(0,int(coordinate[0][0]-2*sep)),0)
+coordinate[1]=(min(dst.shape[1],int(coordinate[1][0]+3.5*sep)),dst.shape[0])
+
+
+
+block_cut = np.zeros(dst.shape)
+
+# print block_cut.shape,"blockcut"
+# print dst.shape,"dst"
+block_cut[coordinate[0][1]:coordinate[1][1],coordinate[0][0]:coordinate[1][0],:] = 1
+# print block_cut.shape,"blockcut"
+dst2=grub_cut(np.copy(dst.astype(np.uint8)),coordinate)
+dst2=dst2*block_cut
+cv2.rectangle(dst2,(coordinate[0][0],coordinate[0][1]),(coordinate[1][0],coordinate[1][1]),(255,0,0),2)
 cv2.waitKey(0)
 cv2.imwrite('dst.jpg',dst)
-cv2.imwrite('image.jpg',image)
+cv2.imwrite('image.jpg',dst2)
+# x_range = np.arange(coordinate[0][0], coordinate[1][0]+1)
+# y_range = np.arange(coordinate[0][1], coordinate[1][1]+1)
+# mesh = np.meshgrid(y_range, x_range)
+# temp = np.zeros(dst.shape)
+# temp[mesh] = 1
+# dst = np.multiply(dst, temp)
  
 # close all open windows
-cv2.destroyAllWindows()
